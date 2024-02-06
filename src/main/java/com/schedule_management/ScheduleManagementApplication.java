0f97ff27698d0646571dfb65c9ddaf505fbe1903
2-seulgi <90025449+2-seulgi@users.noreply.github.com>
2024-02-07 00:58:39 +0900

    #2 - 이벤트를 생성해서 출력해서 확인
    이벤트를 list에 추가하고 출력, 또한 선택한 값만 출력이 되도록 Enum 사용

--- src/main/java/com/schedule_management/ScheduleManagementApplication.java
+++ src/main/java/com/schedule_management/ScheduleManagementApplication.java
@@ -1,13 +1,44 @@
 package com.schedule_management;
 
+import com.schedule_management.event.*;
 import org.springframework.boot.SpringApplication;
 import org.springframework.boot.autoconfigure.SpringBootApplication;
+
+import java.time.ZonedDateTime;
+import java.util.ArrayList;
+import java.util.HashMap;
+import java.util.HashSet;
+import java.util.List;
+import java.util.stream.Collectors;
 
 @SpringBootApplication
 public class ScheduleManagementApplication {
 
     public static void main(String[] args) {
-        SpringApplication.run(ScheduleManagementApplication.class, args);
+        List<AbstractEvent> list = new ArrayList<>();
+        HashSet<String> participants = new HashSet<>();
+        participants.add("bien");
+
+        Meeting meeting1 = new Meeting(
+                1, "meeting1",
+                ZonedDateTime.now(), ZonedDateTime.now().plusHours(1),
+                participants, "meetingRoomA",
+                "스터디"
+        );
+        list.add(meeting1);
+
+        Todo todo1 = new Todo(
+                2, "todo1",
+                ZonedDateTime.now(), ZonedDateTime.now().plusHours(2),
+                "근태조정신청"
+        );
+        list.add(todo1);
+
+        list.forEach(Event::print);
+
+        list.stream().filter(each-> each.support(EventType.MEETING))
+                .forEach(Event::print);
+
     }
 
 }
